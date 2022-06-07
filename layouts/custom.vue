9c28b6d5d570249fcc98486366259cf022f02fba
nakamura196 <na.kamura.1263@gmail.com>
2022-06-07 19:01:40 +0900

    feat: intiail commit

---
+++ layouts/custom.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-app>
+      <v-app-bar id="bar" dark app color="success" flat>
+      <v-toolbar-title>
+        <nuxt-link
+          :to="
+            {
+              name: 'index',
+            }
+          "
+          style="color: inherit; text-decoration: inherit"
+        >
+          {{ "Nuxt 3 x Vuetify 3" }}
+        </nuxt-link>
+      </v-toolbar-title>
+      </v-app-bar>
+    <v-main>
+      <v-container>
+        <slot />
+      </v-container>
+    </v-main>
+    <v-footer
+    class="bg-grey-lighten-1 mt-10"
+  >
+    <v-row
+      justify="center"
+      no-gutters
+    >
+      <v-col
+        class="text-center text-white my-4"
+        cols="12"
+      >
+        {{ new Date().getFullYear() }} — <strong>Satoru Nakamura</strong>
+      </v-col>
+    </v-row>
+  </v-footer>
+  </v-app>
+</template>
